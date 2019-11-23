fc02660dcb82fe96176d6db26d65a243c442510a
pizhou <1983746045@qq.com>
2019-11-23 22:43:31 +0800

    Lombok的三种构造方法的使用

---
+++ src/main/java/com/imooc/pz/Lombok/ConstructorTest.java
@@ -0,0 +1,19 @@
+package com.imooc.pz.Lombok;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+/**
+ * AllArgsConstructor
+ * NoArgsConstructor
+ * RequiredArgsConstructor---部分构造方法
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConstructorTest {
+
+    private String file;
+
+    private String a;
+
+}
